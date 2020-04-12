d8ee9188899b73069c1164aac7bfc50d2ba0e1cd
inventivetalent <mail@inventivetalent.org>
2020-04-13 01:03:02 +0200

    replace http stuff with OkHttp

--- src/main/java/com/sendgrid/Client.java
+++ src/main/java/com/sendgrid/Client.java
@@ -1,386 +1,220 @@
 package com.sendgrid;
 
+import okhttp3.*;
+
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
-
-import org.apache.http.Header;
-import org.apache.http.HttpMessage;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPatch;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-
-
-/**
- * Hack to get DELETE to accept a request body.
- */
-class HttpDeleteWithBody extends HttpEntityEnclosingRequestBase {
-	public static final String METHOD_NAME = "DELETE";
-
-	@Override
-	public String getMethod() {
-		return METHOD_NAME;
-	}
-
-	public HttpDeleteWithBody(final String uri) {
-		super();
-		setURI(URI.create(uri));
-	}
-}
-
 
 /**
  * Class Client allows for quick and easy access any REST or REST-like API.
  */
 public class Client implements Closeable {
 
-	private CloseableHttpClient httpClient;
-	private Boolean test;
-	private boolean createdHttpClient;
-
+	private OkHttpClient httpClient;
+	private Boolean      test;
+	private boolean      createdHttpClient;
 
 	/**
 	 * Constructor for using the default CloseableHttpClient.
 	 */
 	public Client() {
-		this.httpClient = HttpClients.createDefault();
+		this.httpClient = new OkHttpClient.Builder().build();
 		this.test = false;
 		this.createdHttpClient = true;
 	}
 
-
 	/**
-	 * Constructor for passing in an httpClient, typically for mocking. Passed-in httpClient will not be closed
-	 * by this Client.
+	 * Constructor for passing in an httpClient, typically for mocking. Passed-in httpClient will not be closed by this Client.
 	 *
-	 * @param httpClient
-	 *            an Apache CloseableHttpClient
+	 * @param httpClient an Apache CloseableHttpClient
 	 */
-	public Client(CloseableHttpClient httpClient) {
+	public Client(OkHttpClient httpClient) {
 		this(httpClient, false);
 	}
-
 
 	/**
 	 * Constructor for passing in a test parameter to allow for http calls.
 	 *
-	 * @param test
-	 *            is a Bool
+	 * @param test is a Bool
 	 */
 	public Client(Boolean test) {
-		this(HttpClients.createDefault(), test);
+		this(new OkHttpClient.Builder().build(), test);
 	}
-
 
 	/**
 	 * Constructor for passing in an httpClient and test parameter to allow for http calls.
 	 *
-	 * @param httpClient
-	 *            an Apache CloseableHttpClient
-	 * @param test
-	 *            is a Bool
+	 * @param httpClient an Apache CloseableHttpClient
+	 * @param test       is a Bool
 	 */
-	public Client(CloseableHttpClient httpClient, Boolean test) {
+	public Client(OkHttpClient httpClient, Boolean test) {
 		this.httpClient = httpClient;
 		this.test = test;
 		this.createdHttpClient = true;
 	}
 
-
 	/**
 	 * Add query parameters to a URL.
 	 *
-	 * @param baseUri
-	 *            (e.g. "api.sendgrid.com")
-	 * @param endpoint
-	 *            (e.g. "/your/endpoint/path")
-	 * @param queryParams
-	 *            map of key, values representing the query parameters
-	 * @throws URISyntaxException
-	 *            in of a URI syntax error
+	 * @param baseUri     (e.g. "api.sendgrid.com")
+	 * @param endpoint    (e.g. "/your/endpoint/path")
+	 * @param queryParams map of key, values representing the query parameters
+	 * @throws URISyntaxException in of a URI syntax error
 	 */
-	public URI buildUri(String baseUri, String endpoint, Map<String, String> queryParams) throws URISyntaxException {
-		URIBuilder builder = new URIBuilder();
-		URI uri = null;
+	public HttpUrl buildUri(String baseUri, String endpoint, Map<String, String> queryParams) throws URISyntaxException {
+		HttpUrl.Builder httpUrlBuilder = new HttpUrl.Builder();
+		StringBuilder urlString = new StringBuilder();
 
-		if (this.test == true) {
-			builder.setScheme("http");
+		if (this.test) {
+			httpUrlBuilder.scheme("http");
 		} else {
-			builder.setScheme("https");
+			httpUrlBuilder.scheme("https");
 		}
 
-		builder.setHost(baseUri);
-		builder.setPath(endpoint);
+		httpUrlBuilder.host(baseUri);
+		httpUrlBuilder.encodedPath(endpoint);
 
 		if (queryParams != null) {
 			for (Map.Entry<String, String> entry : queryParams.entrySet()) {
-				builder.setParameter(entry.getKey(), entry.getValue());
+				httpUrlBuilder.addQueryParameter(entry.getKey(), entry.getValue());
 			}
 		}
 
-		try {
-			uri = builder.build();
-		} catch (URISyntaxException ex) {
-			throw ex;
-		}
-
-		return uri;
+		return httpUrlBuilder.build();
 	}
-
 
 	/**
 	 * Prepare a Response object from an API call via Apache's HTTP client.
 	 *
-	 * @param response
-	 *            from a call to a CloseableHttpClient
-	 * @throws IOException
-	 *            in case of a network error
+	 * @param response from a call to a CloseableHttpClient
 	 * @return the response object
+	 * @throws IOException in case of a network error
 	 */
-	public Response getResponse(CloseableHttpResponse response) throws IOException {
-		ResponseHandler<String> handler = new SendGridResponseHandler();
-		String responseBody = handler.handleResponse(response);
+	public Response getResponse(okhttp3.Response response) throws IOException {
+		ResponseBody responseBody = response.body();
+		if (responseBody == null) { return null; }
+		String responseBodyString = responseBody.string();
 
-		int statusCode = response.getStatusLine().getStatusCode();
+		int statusCode = response.code();
 
-		Header[] headers = response.getAllHeaders();
+		Headers headers = response.headers();
 		Map<String, String> responseHeaders = new HashMap<String, String>();
-		for (Header h : headers) {
-			responseHeaders.put(h.getName(), h.getValue());
+		for (String name : headers.names()) {
+			responseHeaders.put(name, headers.get(name));
 		}
 
-		return new Response(statusCode, responseBody, responseHeaders);
+		return new Response(statusCode, responseBodyString, responseHeaders);
 	}
 
+	public okhttp3.Request.Builder req(Request request) throws URISyntaxException, IOException {
+		Headers.Builder headersBuilder = new Headers.Builder();
+
+		if (request.getHeaders() != null) {
+			for (Map.Entry<String, String> entry : request.getHeaders().entrySet()) {
+				headersBuilder.set(entry.getKey(), entry.getValue());
+			}
+		}
+
+		HttpUrl uri = buildUri(request.getBaseUri(), request.getEndpoint(), request.getQueryParams());
+		return new okhttp3.Request.Builder()				.url(uri).headers(headersBuilder.build());
+	}
 
 	/**
-	 * Make a GET request and provide the status code, response body and
-	 * response headers.
-	 * 
-	 * @param request 
-	 *            the request object
-	 * @throws URISyntaxException
-	 *            in case of a URI syntax error
-	 * @throws IOException
-	 *            in case of a network error
+	 * Make a GET request and provide the status code, response body and response headers.
+	 *
+	 * @param request the request object
 	 * @return the response object
+	 * @throws URISyntaxException in case of a URI syntax error
+	 * @throws IOException        in case of a network error
 	 */
 	public Response get(Request request) throws URISyntaxException, IOException {
-		URI uri = null;
-		HttpGet httpGet = null;
-
-		try {
-			uri = buildUri(request.getBaseUri(), request.getEndpoint(), request.getQueryParams());
-			httpGet = new HttpGet(uri.toString());
-		} catch (URISyntaxException ex) {
-			throw ex;
-		}
-
-		if (request.getHeaders() != null) {
-			for (Map.Entry<String, String> entry : request.getHeaders().entrySet()) {
-				httpGet.setHeader(entry.getKey(), entry.getValue());
-			}
-		}
-		return executeApiCall(httpGet);
+		okhttp3.Request req = req(request).get().build();
+		return executeApiCall(req);
 	}
 
-
 	/**
-	 * Make a POST request and provide the status code, response body and
-	 * response headers.
+	 * Make a POST request and provide the status code, response body and response headers.
 	 *
-	 * @param request 
-	 *            the request object
-	 * @throws URISyntaxException
-	 *            in case of a URI syntax error
-	 * @throws IOException
-	 *            in case of a network error
+	 * @param request the request object
 	 * @return the response object
+	 * @throws URISyntaxException in case of a URI syntax error
+	 * @throws IOException        in case of a network error
 	 */
 	public Response post(Request request) throws URISyntaxException, IOException {
-		URI uri = null;
-		HttpPost httpPost = null;
-
-		try {
-			uri = buildUri(request.getBaseUri(), request.getEndpoint(), request.getQueryParams());
-			httpPost = new HttpPost(uri.toString());
-		} catch (URISyntaxException ex) {
-			throw ex;
-		}
-
-		if (request.getHeaders() != null) {
-			for (Map.Entry<String, String> entry : request.getHeaders().entrySet()) {
-				httpPost.setHeader(entry.getKey(), entry.getValue());
-			}
-		}
-
-		httpPost.setEntity(new StringEntity(request.getBody(), Charset.forName("UTF-8")));
-		writeContentTypeIfNeeded(request, httpPost);
-
-		return executeApiCall(httpPost);
+		RequestBody requestBody = RequestBody.create(request.getBody(), MediaType.get("application/json"));
+		okhttp3.Request req = req(request).post(requestBody).build();
+		return executeApiCall(req);
 	}
 
-
 	/**
-	 * Make a PATCH request and provide the status code, response body and
-	 * response headers.
+	 * Make a PATCH request and provide the status code, response body and response headers.
 	 *
-	 * @param request 
-	 *            the request object
-	 * @throws URISyntaxException
-	 *            in case of a URI syntax error
-	 * @throws IOException
-	 *            in case of a network error
+	 * @param request the request object
 	 * @return the response object
+	 * @throws URISyntaxException in case of a URI syntax error
+	 * @throws IOException        in case of a network error
 	 */
 	public Response patch(Request request) throws URISyntaxException, IOException {
-		URI uri = null;
-		HttpPatch httpPatch = null;
-
-		try {
-			uri = buildUri(request.getBaseUri(), request.getEndpoint(), request.getQueryParams());
-			httpPatch = new HttpPatch(uri.toString());
-		} catch (URISyntaxException ex) {
-			throw ex;
-		}
-
-		if (request.getHeaders() != null) {
-			for (Map.Entry<String, String> entry : request.getHeaders().entrySet()) {
-				httpPatch.setHeader(entry.getKey(), entry.getValue());
-			}
-		}
-
-		httpPatch.setEntity(new StringEntity(request.getBody(), Charset.forName("UTF-8")));
-		writeContentTypeIfNeeded(request, httpPatch);
-
-		return executeApiCall(httpPatch);
+		RequestBody requestBody = RequestBody.create(request.getBody(), MediaType.get("application/json"));
+		okhttp3.Request req = req(request).patch(requestBody).build();
+		return executeApiCall(req);
 	}
-
 
 	/**
-	 * Make a PUT request and provide the status code, response body and
-	 * response headers.
+	 * Make a PUT request and provide the status code, response body and response headers.
 	 *
-	 * @param request 
-	 *            the request object
-	 * @throws URISyntaxException
-	 *            in case of a URI syntax error
-	 * @throws IOException
-	 *            in case of a network error
+	 * @param request the request object
 	 * @return the response object
+	 * @throws URISyntaxException in case of a URI syntax error
+	 * @throws IOException        in case of a network error
 	 */
 	public Response put(Request request) throws URISyntaxException, IOException {
-		URI uri = null;
-		HttpPut httpPut = null;
-
-		try {
-			uri = buildUri(request.getBaseUri(), request.getEndpoint(), request.getQueryParams());
-			httpPut = new HttpPut(uri.toString());
-		} catch (URISyntaxException ex) {
-			throw ex;
-		}
-
-		if (request.getHeaders() != null) {
-			for (Map.Entry<String, String> entry : request.getHeaders().entrySet()) {
-				httpPut.setHeader(entry.getKey(), entry.getValue());
-			}
-		}
-
-		httpPut.setEntity(new StringEntity(request.getBody(), Charset.forName("UTF-8")));
-		writeContentTypeIfNeeded(request, httpPut);
-
-		return executeApiCall(httpPut);
+		RequestBody requestBody = RequestBody.create(request.getBody(), MediaType.get("application/json"));
+		okhttp3.Request req = req(request).put(requestBody).build();
+		return executeApiCall(req);
 	}
-
 
 	/**
 	 * Make a DELETE request and provide the status code and response headers.
 	 *
-	 * @param request 
-	 *            the request object
-	 * @throws URISyntaxException
-	 *            in case of a URI syntax error
-	 * @throws IOException
-	 *            in case of a network error
+	 * @param request the request object
 	 * @return the response object
+	 * @throws URISyntaxException in case of a URI syntax error
+	 * @throws IOException        in case of a network error
 	 */
 	public Response delete(Request request) throws URISyntaxException, IOException {
-		URI uri = null;
-		HttpDeleteWithBody httpDelete = null;
-
-		try {
-			uri = buildUri(request.getBaseUri(), request.getEndpoint(), request.getQueryParams());
-			httpDelete = new HttpDeleteWithBody(uri.toString());
-		} catch (URISyntaxException ex) {
-			throw ex;
-		}
-
-		if (request.getHeaders() != null) {
-			for (Map.Entry<String, String> entry : request.getHeaders().entrySet()) {
-				httpDelete.setHeader(entry.getKey(), entry.getValue());
-			}
-		}
-
-		httpDelete.setEntity(new StringEntity(request.getBody(), Charset.forName("UTF-8")));
-		writeContentTypeIfNeeded(request, httpDelete);
-
-		return executeApiCall(httpDelete);
+		RequestBody requestBody = RequestBody.create(request.getBody(), MediaType.get("application/json"));
+		okhttp3.Request req = req(request).delete(requestBody).build();
+		return executeApiCall(req);
 	}
-
-	private void writeContentTypeIfNeeded(Request request, HttpMessage httpMessage) {
-		if (!"".equals(request.getBody())) {
-			httpMessage.setHeader("Content-Type", "application/json");
-		}
-	}
-
 
 	/**
 	 * Makes a call to the client API.
 	 *
-	 * @param httpPost 
-	 *            the request method object
-	 * @throws IOException
-	 *            in case of a network error
+	 * @param request the request method object
 	 * @return the response object
+	 * @throws IOException in case of a network error
 	 */
-	private Response executeApiCall(HttpRequestBase httpPost) throws IOException {
+	private Response executeApiCall(okhttp3.Request request) throws IOException {
+		okhttp3.Response serverResponse = httpClient.newCall(request).execute();
 		try {
-			CloseableHttpResponse serverResponse = httpClient.execute(httpPost);
-			try {
-				return getResponse(serverResponse);
-			} finally {
-				serverResponse.close();
-			}
-		} catch(ClientProtocolException e) {
-			throw new IOException(e.getMessage());
+			return getResponse(serverResponse);
+		} finally {
+			serverResponse.close();
 		}
 	}
-
 
 	/**
 	 * A thin wrapper around the HTTP methods.
 	 *
-	 * @param request 
-	 *            the request object
-	 * @throws IOException
-	 *            in case of a network error
+	 * @param request the request object
 	 * @return the response object
+	 * @throws IOException in case of a network error
 	 */
 	public Response api(Request request) throws IOException {
 		try {
@@ -388,18 +222,18 @@ public class Client implements Closeable {
 				throw new IOException("We only support GET, PUT, PATCH, POST and DELETE.");
 			}
 			switch (request.getMethod()) {
-			case GET:
-				return get(request);
-			case POST:
-				return post(request);
-			case PUT:
-				return put(request);
-			case PATCH:
-				return patch(request);
-			case DELETE:
-				return delete(request);
-			default:
-				throw new IOException("We only support GET, PUT, PATCH, POST and DELETE.");
+				case GET:
+					return get(request);
+				case POST:
+					return post(request);
+				case PUT:
+					return put(request);
+				case PATCH:
+					return patch(request);
+				case DELETE:
+					return delete(request);
+				default:
+					throw new IOException("We only support GET, PUT, PATCH, POST and DELETE.");
 			}
 		} catch (IOException ex) {
 			throw ex;
@@ -410,30 +244,25 @@ public class Client implements Closeable {
 		}
 	}
 
-
 	/**
 	 * Closes the http client.
 	 *
-	 * @throws IOException
-	 *            in case of a network error
-	 */	
+	 * @throws IOException in case of a network error
+	 */
 	@Override
 	public void close() throws IOException {
-      	this.httpClient.close();
 	}
-
 
 	/**
 	 * Closes and finalizes the http client.
 	 *
-	 * @throws Throwable
-	 *            in case of an error
-	 */		
+	 * @throws Throwable in case of an error
+	 */
 	@Override
 	public void finalize() throws Throwable {
 		try {
 			close();
-		} catch(IOException e) {
+		} catch (IOException e) {
 			throw new Throwable(e.getMessage());
 		} finally {
 			super.finalize();
